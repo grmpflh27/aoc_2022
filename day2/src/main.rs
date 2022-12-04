ee15b5c5f5f36c22932afedf08730b5f1b2d02da
Thomas Beyer <grmpflh27@gmail.com>
2022-12-03 22:40:30 -0800

    rustfmt day1 + day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,41 +1,39 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 
 const DAY: &str = "day2";
 
-#[derive(PartialEq)]
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 enum RPSType {
     Rock,
     Paper,
-    Scissors
+    Scissors,
 }
 
 struct RPS {
     id: RPSType,
-    value: u32
+    value: u32,
 }
 
-const rock: RPS = RPS{
+const rock: RPS = RPS {
     id: RPSType::Rock,
-    value: 1
+    value: 1,
 };
-const paper: RPS = RPS{
+const paper: RPS = RPS {
     id: RPSType::Paper,
-    value: 2
+    value: 2,
 };
-const scissors: RPS = RPS{
+const scissors: RPS = RPS {
     id: RPSType::Scissors,
-    value: 3
+    value: 3,
 };
 
-#[derive(Debug)]
-#[derive(PartialEq)]
-enum Outcome{
+#[derive(Debug, PartialEq)]
+enum Outcome {
     Win,
     Loss,
-    Draw
+    Draw,
 }
 
 fn main() {
@@ -46,29 +44,27 @@ fn main() {
     part2(&strategy_guide);
 }
 
-fn read_lines(filename: &str) -> String{
+fn read_lines(filename: &str) -> String {
     let root_dir = env::var("AOC_ROOT_DIR").expect("$AOC_ROOT_DIR is not set");
     let abs_filename = format!("{}{}", root_dir, filename);
-    
+
     println!("Reading {}", abs_filename);
-    let contents = fs::read_to_string(abs_filename)
-       .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(abs_filename).expect("Something went wrong reading the file");
 
-    return contents
+    return contents;
 }
-
 
 fn part1(strategy_guide: &str) {
     let code_book = get_code_book_part1();
     let mut total_score: u32 = 0;
 
-    for line in strategy_guide.lines(){
+    for line in strategy_guide.lines() {
         let other = line.chars().nth(0).unwrap();
         let otherInstr = &code_book[&other];
-        
+
         let mine = line.chars().nth(2).unwrap();
         let mineInstr = &code_book[&mine];
-        
+
         total_score += get_score(mineInstr, otherInstr);
     }
     println!("part 1: {}", total_score);
@@ -85,21 +81,21 @@ fn get_code_book_part1() -> HashMap<char, RPS> {
     ]);
 }
 
-fn get_score(mine: &RPS, other: &RPS) -> u32{
+fn get_score(mine: &RPS, other: &RPS) -> u32 {
     let mut outcome: Outcome = Outcome::Loss;
 
-    if mine.id == other.id{
+    if mine.id == other.id {
         outcome = Outcome::Draw;
     } else if mine.id == RPSType::Rock {
-        if other.id == RPSType::Scissors{
+        if other.id == RPSType::Scissors {
             outcome = Outcome::Win;
         }
-    } else if  mine.id == RPSType::Paper {
-        if other.id == RPSType::Rock{
+    } else if mine.id == RPSType::Paper {
+        if other.id == RPSType::Rock {
             outcome = Outcome::Win;
         }
     } else if mine.id == RPSType::Scissors {
-        if other.id == RPSType::Paper{
+        if other.id == RPSType::Paper {
             outcome = Outcome::Win;
         }
     }
@@ -119,24 +115,20 @@ fn part2(strategy_guide: &str) {
     let code_outcomes = get_code_book_outcomes();
     let mut total_score: u32 = 0;
 
-    for line in strategy_guide.lines(){
+    for line in strategy_guide.lines() {
         let other = line.chars().nth(0).unwrap();
         let otherInstr = &code_book[&other];
-        
+
         let outcome_code = line.chars().nth(2).unwrap();
         let outcome = &code_outcomes[&outcome_code];
-        
+
         total_score += get_score_part2(outcome, otherInstr);
     }
     println!("part 2: {}", total_score);
 }
 
 fn get_code_book_part2() -> HashMap<char, RPS> {
-    return HashMap::from([
-        ('A', rock),
-        ('B', paper),
-        ('C', scissors),
-    ]);
+    return HashMap::from([('A', rock), ('B', paper), ('C', scissors)]);
 }
 
 fn get_code_book_outcomes() -> HashMap<char, Outcome> {
@@ -147,17 +139,29 @@ fn get_code_book_outcomes() -> HashMap<char, Outcome> {
     ]);
 }
 
-fn get_score_part2(outcome: &Outcome, other: &RPS) -> u32{
+fn get_score_part2(outcome: &Outcome, other: &RPS) -> u32 {
     let mut value = 0;
 
-    if outcome == &Outcome::Draw{
+    if outcome == &Outcome::Draw {
         value = other.value;
     } else if other.id == RPSType::Rock {
-        value = if outcome == &Outcome::Win { paper.value } else { scissors.value };
+        value = if outcome == &Outcome::Win {
+            paper.value
+        } else {
+            scissors.value
+        };
     } else if other.id == RPSType::Paper {
-        value = if outcome == &Outcome::Win { scissors.value } else { rock.value };
+        value = if outcome == &Outcome::Win {
+            scissors.value
+        } else {
+            rock.value
+        };
     } else if other.id == RPSType::Scissors {
-        value = if outcome == &Outcome::Win { rock.value } else { paper.value };
+        value = if outcome == &Outcome::Win {
+            rock.value
+        } else {
+            paper.value
+        };
     }
 
     let mut score: u32 = match outcome {
